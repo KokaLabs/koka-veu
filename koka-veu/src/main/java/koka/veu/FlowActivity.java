195d0ac1a5fa0b55e962b7dd0fcda7b79ae83efa
Scotty Tomaszewski <scottTomaszewski@gmail.com>
2013-12-10 18:35:01 -0500

    Removes comments

--- koka-veu/src/main/java/koka/veu/FlowActivity.java
+++ koka-veu/src/main/java/koka/veu/FlowActivity.java
@@ -21,22 +21,9 @@ public class FlowActivity extends FragmentActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.flow);
-
-        // Show the Up button in the action bar.
         getActionBar().setDisplayHomeAsUpEnabled(true);
 
-        // savedInstanceState is non-null when there is fragment state
-        // saved from previous configurations of this activity
-        // (e.g. when rotating the screen from portrait to landscape).
-        // In this case, the fragment will automatically be re-added
-        // to its container so we don't need to manually add it.
-        // For more information, see the Fragments API guide at:
-        //
-        // http://developer.android.com/guide/components/fragments.html
-        //
         if (savedInstanceState == null) {
-            // Create the detail fragment and add it to the activity
-            // using a fragment transaction.
             FlowFragment fragment = new FlowFragment();
             getSupportFragmentManager().beginTransaction()
                     .add(R.id.flow_container, fragment)
