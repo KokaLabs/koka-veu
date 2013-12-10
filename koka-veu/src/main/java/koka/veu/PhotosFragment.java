a02b11a84b0b578b421f3b08980451febcfcc153
Scotty Tomaszewski <scottTomaszewski@gmail.com>
2013-12-10 17:34:53 -0500

    Sets up callbacks for each view when photo selected

--- koka-veu/src/main/java/koka/veu/PhotosFragment.java
+++ koka-veu/src/main/java/koka/veu/PhotosFragment.java
@@ -2,10 +2,13 @@ package koka.veu;
 
 import android.app.Fragment;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.GridView;
-import android.widget.ListView;
+import android.widget.Toast;
 
-public class PhotosFragment extends Fragment {
+public abstract class PhotosFragment extends Fragment {
   protected GridView gridview;
 
   @Override
@@ -13,7 +16,11 @@ public class PhotosFragment extends Fragment {
     super.onCreate(savedInstanceState);
     gridview = (GridView) this.getActivity().findViewById(R.id.photogrid);
     gridview.setAdapter(new PhotoImageAdapter(this.getActivity()));
+
+    gridview.setOnItemClickListener(onPhotoClick());
   }
+
+  protected abstract AdapterView.OnItemClickListener onPhotoClick();
 
   @Override
   public void onActivityCreated(Bundle savedInstanceState) {
@@ -30,6 +37,15 @@ public class PhotosFragment extends Fragment {
       super.onCreate(savedInstanceState);
       gridview.setChoiceMode(GridView.CHOICE_MODE_SINGLE);
     }
+
+    @Override
+    protected AdapterView.OnItemClickListener onPhotoClick() {
+      return new AdapterView.OnItemClickListener() {
+        public void onItemClick(AdapterView<?> parent, View v, int position, long id) {
+          Toast.makeText(getActivity(), "" + position, Toast.LENGTH_SHORT).show();
+        }
+      };
+    }
   }
 
   public static final class SinglePanePhotosFragment extends PhotosFragment {
@@ -37,6 +53,15 @@ public class PhotosFragment extends Fragment {
     public void onCreate(Bundle savedInstanceState) {
       super.onCreate(savedInstanceState);
       gridview.setChoiceMode(GridView.CHOICE_MODE_NONE);
+    }
+
+    @Override
+    protected AdapterView.OnItemClickListener onPhotoClick() {
+      return new AdapterView.OnItemClickListener() {
+        public void onItemClick(AdapterView<?> parent, View v, int position, long id) {
+          Toast.makeText(getActivity(), "" + position, Toast.LENGTH_SHORT).show();
+        }
+      };
     }
   }
 }
