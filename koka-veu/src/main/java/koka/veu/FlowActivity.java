40d597364f0d995699e0ae474f0bdc7eee4c92f5
Scotty Tomaszewski <scottTomaszewski@gmail.com>
2013-12-10 18:35:41 -0500

    Formatting

--- koka-veu/src/main/java/koka/veu/FlowActivity.java
+++ koka-veu/src/main/java/koka/veu/FlowActivity.java
@@ -11,40 +11,39 @@ import android.view.MenuItem;
  * activity is only used on handset devices. On tablet-size devices,
  * item details are presented side-by-side with a list of items
  * in a {@link koka.veu.ItemListActivity}.
- * <p>
+ * <p/>
  * This activity is mostly just a 'shell' activity containing nothing
  * more than a {@link koka.veu.ItemDetailFragment}.
  */
 public class FlowActivity extends FragmentActivity {
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.flow);
+    getActionBar().setDisplayHomeAsUpEnabled(true);
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.flow);
-        getActionBar().setDisplayHomeAsUpEnabled(true);
-
-        if (savedInstanceState == null) {
-            FlowFragment fragment = new FlowFragment();
-            getSupportFragmentManager().beginTransaction()
-                    .add(R.id.flow_container, fragment)
-                    .commit();
-        }
+    if (savedInstanceState == null) {
+      FlowFragment fragment = new FlowFragment();
+      getSupportFragmentManager().beginTransaction()
+          .add(R.id.flow_container, fragment)
+          .commit();
     }
+  }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()) {
-            case android.R.id.home:
-                // This ID represents the Home or Up button. In the case of this
-                // activity, the Up button is shown. Use NavUtils to allow users
-                // to navigate up one level in the application structure. For
-                // more details, see the Navigation pattern on Android Design:
-                //
-                // http://developer.android.com/design/patterns/navigation.html#up-vs-back
-                //
-                NavUtils.navigateUpTo(this, new Intent(this, ItemListActivity.class));
-                return true;
-        }
-        return super.onOptionsItemSelected(item);
+  @Override
+  public boolean onOptionsItemSelected(MenuItem item) {
+    switch (item.getItemId()) {
+      case android.R.id.home:
+        // This ID represents the Home or Up button. In the case of this
+        // activity, the Up button is shown. Use NavUtils to allow users
+        // to navigate up one level in the application structure. For
+        // more details, see the Navigation pattern on Android Design:
+        //
+        // http://developer.android.com/design/patterns/navigation.html#up-vs-back
+        //
+        NavUtils.navigateUpTo(this, new Intent(this, ItemListActivity.class));
+        return true;
     }
+    return super.onOptionsItemSelected(item);
+  }
 }
