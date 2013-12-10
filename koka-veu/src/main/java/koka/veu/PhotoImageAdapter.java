b10c8a57323c596be08485d27e61817565b5dbe2
Scotty Tomaszewski <scottTomaszewski@gmail.com>
2013-12-10 15:08:22 -0500

    formatting

--- koka-veu/src/main/java/koka/veu/PhotoImageAdapter.java
+++ koka-veu/src/main/java/koka/veu/PhotoImageAdapter.java
@@ -8,50 +8,50 @@ import android.widget.GridView;
 import android.widget.ImageView;
 
 public class PhotoImageAdapter extends BaseAdapter {
-    private Context mContext;
+  private Context mContext;
 
-    public  PhotoImageAdapter(Context c) {
-        mContext = c;
+  public PhotoImageAdapter(Context c) {
+    mContext = c;
+  }
+
+  public int getCount() {
+    return mThumbIds.length;
+  }
+
+  public Object getItem(int position) {
+    return null;
+  }
+
+  public long getItemId(int position) {
+    return 0;
+  }
+
+  public View getView(int position, View convertView, ViewGroup parent) {
+    ImageView imageView;
+    if (convertView == null) {  // if it's not recycled, initialize some attributes
+      imageView = new ImageView(mContext);
+      imageView.setLayoutParams(new GridView.LayoutParams(85, 85));
+      imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
+      imageView.setPadding(8, 8, 8, 8);
+    } else {
+      imageView = (ImageView) convertView;
     }
 
-    public int getCount() {
-        return mThumbIds.length;
-    }
+    imageView.setImageResource(mThumbIds[position]);
+    return imageView;
+  }
 
-    public Object getItem(int position) {
-        return null;
-    }
-
-    public long getItemId(int position) {
-        return 0;
-    }
-
-    public View getView(int position, View convertView, ViewGroup parent) {
-        ImageView imageView;
-        if (convertView == null) {  // if it's not recycled, initialize some attributes
-            imageView = new ImageView(mContext);
-            imageView.setLayoutParams(new GridView.LayoutParams(85, 85));
-            imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
-            imageView.setPadding(8, 8, 8, 8);
-        } else {
-            imageView = (ImageView) convertView;
-        }
-
-        imageView.setImageResource(mThumbIds[position]);
-        return imageView;
-    }
-
-    private Integer[] mThumbIds = {
-            R.drawable.sample_2, R.drawable.sample_3,
-            R.drawable.sample_4, R.drawable.sample_5,
-            R.drawable.sample_6, R.drawable.sample_7,
-            R.drawable.sample_0, R.drawable.sample_1,
-            R.drawable.sample_2, R.drawable.sample_3,
-            R.drawable.sample_4, R.drawable.sample_5,
-            R.drawable.sample_6, R.drawable.sample_7,
-            R.drawable.sample_0, R.drawable.sample_1,
-            R.drawable.sample_2, R.drawable.sample_3,
-            R.drawable.sample_4, R.drawable.sample_5,
-            R.drawable.sample_6, R.drawable.sample_7
-    };
+  private Integer[] mThumbIds = {
+      R.drawable.sample_2, R.drawable.sample_3,
+      R.drawable.sample_4, R.drawable.sample_5,
+      R.drawable.sample_6, R.drawable.sample_7,
+      R.drawable.sample_0, R.drawable.sample_1,
+      R.drawable.sample_2, R.drawable.sample_3,
+      R.drawable.sample_4, R.drawable.sample_5,
+      R.drawable.sample_6, R.drawable.sample_7,
+      R.drawable.sample_0, R.drawable.sample_1,
+      R.drawable.sample_2, R.drawable.sample_3,
+      R.drawable.sample_4, R.drawable.sample_5,
+      R.drawable.sample_6, R.drawable.sample_7
+  };
 }
