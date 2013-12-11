4ba9b73946ef03a3269f0efc3f4954005b954140
Scotty Tomaszewski <scottTomaszewski@gmail.com>
2013-12-10 21:31:05 -0500

    Fixed critical bug
    Should result in 92% higher satisfaction rate among users

--- koka-veu/src/main/java/koka/veu/SquareImageView.java
+++ koka-veu/src/main/java/koka/veu/SquareImageView.java
@@ -5,7 +5,6 @@ import android.util.AttributeSet;
 import android.widget.ImageView;
 
 public class SquareImageView extends ImageView {
-
   public SquareImageView(final Context context) {
     super(context);
   }
